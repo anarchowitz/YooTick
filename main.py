645ace6fe0329a3d706e7047738e6a61e9c7591e
anarchowitz <anarchowitz>
2025-03-15 14:23:11 +0300

    changed actually version

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastcommands import setupfastcommands
 
 intents = disnake.Intents.default() 
 intents.message_content = True
-version = "3.4-B"
+version = "3.4-S"
 bot = commands.Bot(command_prefix="/", intents=intents, activity=disnake.Activity(type=disnake.ActivityType.listening, name=f"yooma.su | v{version}"))
 
 db = Database("database.db")
