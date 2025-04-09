faf315f94b3eedccd6778ed4c49522afad17fd49
anarchowitz <anarchowitz>
2025-04-09 13:52:01 +0300

    global fix / reworked add keywords fixed code

--- moderator.py
+++ moderator.py
@@ -1,40 +1,41 @@
-import re, disnake
+import re
+import disnake
 from database import Database
 
 class Moderator:
     def __init__(self):
         self.db = Database("database.db")
         self.keywords = [
-            "free cheat",
-            "join server",
-            "discord invite",
-            "free gift",
-            "steam gift",
-            "steam free",
-            "free discord nitro",
-            "free steam",
-            "discord link",
-            "server invite",
-            "join now",
-            "onlyfans leaks",
-            "personal gift",
-            "click here for nsfw content",
-            "scan this qr code",
-            "get exclusive nsfw content",
-            "free giveaway",
-            "check what",
-            "join for free",
-            "free porn",
-            "fire porn"
-        ]
-        self.allowed_domains = [
-            "discord.gg",
-            "yooma.su"
+            r"cybershoke\.net",
+            r"cs2red\.ru",
+            r"onetake-cs2\.ru",
+            r"discord\.com\/invite\/",
+            r"discord\.com\/invites\/",
+            r"discord\.io\/",
+            r"discord\.me\/",
+            r"onlyfans",
+            r"free",
+            r"gift",
+            r"giveaway",
+            r"join",
+            r"server",
+            r"invite",
+            r"link",
+            r"nsfw",
+            r"porn",
+            r"leaks",
+            r"qr code",
+            r"exclusive",
+            r"personal",
+            r"wet",
+            r"fire",
+            r"com server",
+            r"cs2"
         ]
         self.patterns = [re.compile(keyword, re.IGNORECASE) for keyword in self.keywords]
 
     def check_message(self, message):
-        if isinstance(message.channel, (disnake.TextChannel, disnake.VoiceChannel, disnake.NSFWLevel)):
+        if isinstance(message.channel, (disnake.TextChannel, disnake.VoiceChannel, disnake.Thread)):
             self.db.cursor.execute("SELECT * FROM staff_list WHERE user_id = ?", (message.author.id,))
             staff_member = self.db.cursor.fetchone()
             if staff_member is not None:
@@ -42,8 +43,6 @@ class Moderator:
 
             for pattern in self.patterns:
                 if pattern.search(message.content):
-                    for domain in self.allowed_domains:
-                        if domain in message.content:
-                            return False
                     return True
+
             return False
