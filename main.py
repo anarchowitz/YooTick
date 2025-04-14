a2abb852b1262712c01f4646769bc2c920f958ca
anarchowitz <anarchowitz>
2025-04-14 18:46:34 +0300

    changed ver.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from notifications import run_schedule
 intents = disnake.Intents.default() 
 intents.message_content = True
 intents.guilds = True
-version = "3.5.5"
+version = "3.5.6"
 bot = commands.Bot(command_prefix="/", intents=intents, activity=disnake.Activity(type=disnake.ActivityType.listening, name=f"yooma.su | v{version}"))
 
 db = Database("database.db")
