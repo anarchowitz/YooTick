51852d93e34aa24c1d1c1ffc85e15a92b8221616
anarchowitz <anarchowitz>
2025-05-01 19:41:36 +0300

    added new option in staff_list, daily_quota

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ intents = disnake.Intents.default()
 intents.message_content = True
 intents.guilds = True
 intents.members = True
-version = "3.5.8.1"
+version = "3.5.8.2"
 bot = commands.Bot(command_prefix="/", intents=intents, activity=disnake.Activity(type=disnake.ActivityType.listening, name=f"yooma.su | v{version}"))
 
 db = Database("database.db")
